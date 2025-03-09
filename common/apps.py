48cce93bf144e749d7a1c9019786b01bed3d8774
Danilo Macedo Azevedo Fernandes <danilo.macedofernandes@hotmail.com>
2025-03-08 23:21:41 -0300

    :hammer: chore: Removing Signals

--- common/apps.py
+++ common/apps.py
@@ -1,20 +1,6 @@
-from django.apps import AppConfig, apps
-from django.db.models.signals import post_delete
+from django.apps import AppConfig
 
 
 class CommonConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "common"
-
-    def ready(self):
-        """Register signals after all migrations are completed."""
-        self._register_all_base_models()
-
-    def _register_all_base_models(self, **kwargs):
-        from common.services.loggers.app_logger import ApplicationLogger
-        from common.signals import base_model_post_delete
-
-        logger = ApplicationLogger()
-        for model in apps.get_models():
-            logger.info(f"Registering post_delete signal for {model.__name__}")
-            post_delete.connect(base_model_post_delete, sender=model, weak=False)
